5be18de4233e8502cc10acbbe22216f7afeed39a
WindowsMEMZ <memz1@darock.top>
2025-01-03 10:20:49 +0800

    Initial commit

---
+++ DarockFoundation.xcframework/ios-arm64_x86_64-simulator/DarockFoundation.framework/Headers/DarockFoundation.h
@@ -0,0 +1,8 @@
+//
+//  DarockFoundation.h
+//  DarockFoundation
+//
+//  Created by Mark Chan on 2025/1/3.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DarockFoundation",
+    products: [
+        .library(name: "DarockFoundation", targets: ["DarockFoundation"])
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "DarockFoundation", path: "DarockFoundation.xcframework")
+    ]
+)
