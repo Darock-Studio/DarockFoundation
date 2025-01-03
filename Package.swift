b1f234fa94c6930cdbe8e5e8e557e02922426415
WindowsMEMZ <memz1@darock.top>
2025-01-03 10:29:32 +0800

    Update Dependencies

--- Package.swift
+++ Package.swift
@@ -8,6 +8,10 @@ let package = Package(
     products: [
         .library(name: "DarockFoundation", targets: ["DarockFoundation"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.10.2"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.2")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
