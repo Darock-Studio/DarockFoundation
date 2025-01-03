5ac988ffd4f6491dbfacd025d0b3f34fd3400e22
WindowsMEMZ <memz1@darock.top>
2025-01-03 10:50:18 +0800

    Update Dependencies

--- Package.swift
+++ Package.swift
@@ -6,11 +6,19 @@ import PackageDescription
 let package = Package(
     name: "DarockFoundation",
     products: [
-        .library(name: "DarockFoundation", targets: ["DarockFoundation"])
+        .library(name: "DarockFoundation", targets: ["DarockFoundation"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.10.2"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "DarockFoundation", path: "DarockFoundation.xcframework")
+        .binaryTarget(name: "DarockFoundation", path: "DarockFoundation.xcframework"),
+        .target(name: "_DarockFoundationDependenciesWrapper", dependencies: [
+            "Alamofire",
+            "SwiftyJSON"
+        ])
     ]
 )
